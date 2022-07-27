1f6c0c8a227ee00a6ce229d12aa22e7ca433af58
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-07-27 20:00:22 +0530

    Add files via upload

---
+++ .gh05t/package.sh
@@ -0,0 +1,215 @@
+# Code : GH05T HUNTER5
+# Collaborator : GH05T-HUNTER5
+ping -c 1 google.com
+Return="$?"
+if [ "$Return" -eq "0" ]; then
+echo ""
+clear
+else
+clear
+echo ""
+echo " Please On Your Mobile Data"
+sleep 5
+clear
+fi
+clear
+echo -e " \033[1;37m +----------------------------------------------+"
+echo -e " \033[1;37m | \033[0;32m  ██████╗ ██╗  ██╗ ██████╗ ███████╗████████╗\033[1;37m |"
+echo -e " \033[1;37m |\033[0;32m  ██╔════╝ ██║  ██║██╔═████╗██╔════╝╚══██╔══╝\033[1;37m |"
+echo -e " \033[1;37m |\033[0;32m  ██║  ███╗███████║██║██╔██║███████╗   ██║ \033[1;37m   |"
+echo -e " \033[1;37m |\033[0;32m  ██║   ██║██╔══██║████╔╝██║╚════██║   ██║ \033[1;37m   |"
+echo -e " \033[1;37m |\033[0;32m  ╚██████╔╝██║  ██║╚██████╔╝███████║   ██║ \033[1;37m   |"
+echo -e " \033[1;37m | \033[0;32m  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝ \033[1;37m   |"
+echo -e " \033[1;37m +----------------------------------------------+"
+echo -e " \033[1;37m |  \033[0;32m   ██╗███╗   ██╗███████╗████████╗ █████╗  \033[1;37m  |"
+echo -e " \033[1;37m |  \033[0;32m   ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗  \033[1;37m |"
+echo -e " \033[1;37m |  \033[0;32m   ██║██╔██╗ ██║███████╗   ██║   ███████║ \033[1;37m  |"
+echo -e " \033[1;37m |  \033[0;32m   ██║██║╚██╗██║╚════██║   ██║   ██╔══██║  \033[1;37m |"
+echo -e " \033[1;37m |  \033[0;32m   ██║██║ ╚████║███████║   ██║   ██║  ██║ \033[1;37m  |"
+echo -e " \033[1;37m |  \033[0;32m   ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ \033[1;37m  |"
+echo -e " \033[1;37m +----------------------------------------------+"
+echo -e " \033[1;37m |\033[0;31m Developer\033[1;37m :\033[0;34m GH05T HUNTER5   \033[0;31m   version\033[1;37m :\033[0;34m 0.01\033[1;37m|"
+echo -e " \033[1;37m |\033[0;31m Instagram\033[1;37m :\033[0;34m GH05T_HUNTER5   \033[0;31m   FPP    \033[1;37m :\033[0;34m 0.29\033[1;37m|"
+echo -e " \033[1;37m +----------------------------------------------+"
+exit_on_signal_SIGINT() {
+echo " Trying To Exit"
+exit 0
+}
+exit_on_signal_SIGTERM() {
+echo " Trying To Exit"
+sleep 2
+exit 0
+}
+trap exit_on_signal_SIGINT SIGINT
+trap exit_on_signal_SIGTERM SIGTERM
+apt install -y python2 python tor wget > /dev/null 2>&1
+ping -c 1 google.com
+Return="$?"
+if [ "$Return" -eq "0" ]; then
+echo ""
+clear
+else
+clear
+echo ""
+echo " Dude  Please On Your Mobile Data"
+exit
+fi
+dpkg -s tor &> /dev/null # GH05T HUNTER5
+if [ $? -eq 0 ]; then
+echo ""
+clear
+else
+echo ""
+pkg install tor -y
+clear
+fi
+dpkg -s curl &> /dev/null #GH05T HUNTER5
+if [ $? -eq 0 ]; then
+echo ""
+clear
+else
+echo ""
+pkg install curl -y
+clear
+fi
+dpkg -s php &> /dev/null # GH05T HUNTER5
+if [ $? -eq 0 ]; then
+echo ""
+clear
+else
+echo ""
+pkg install php -y
+clear
+fi
+dpkg -s wget &> /dev/null # GH05T HUNTER5
+if [ $? -eq 0 ]; then
+echo ""
+clear
+else
+echo ""
+pkg install wget -y
+pkg install wget
+clear
+fi
+dpkg -s python &> /dev/null # GH05T HUNTER5
+if [ $? -eq 0 ]; then
+echo ""
+clear
+else
+echo ""
+pkg install python -y
+clear
+fi
+dpkg --configure -a 
+pip install --upgrade pip >/dev/null 2>&1
+pip3 install requests --upgrade >/dev/null 2>&1
+pip3 install requests[socks] >/dev/null 2>&1
+pip3 install stem >/dev/null 2>&1
+pip3 install instagram-py >/dev/null 2>&1
+clear
+echo -e " \033[1;37m +----------------------------------------------+"
+echo -e " \033[1;37m | \033[0;32m  ██████╗ ██╗  ██╗ ██████╗ ███████╗████████╗\033[1;37m |"
+echo -e " \033[1;37m |\033[0;32m  ██╔════╝ ██║  ██║██╔═████╗██╔════╝╚══██╔══╝\033[1;37m |"
+echo -e " \033[1;37m |\033[0;32m  ██║  ███╗███████║██║██╔██║███████╗   ██║ \033[1;37m   |"
+echo -e " \033[1;37m |\033[0;32m  ██║   ██║██╔══██║████╔╝██║╚════██║   ██║ \033[1;37m   |"
+echo -e " \033[1;37m |\033[0;32m  ╚██████╔╝██║  ██║╚██████╔╝███████║   ██║ \033[1;37m   |"
+echo -e " \033[1;37m | \033[0;32m  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝ \033[1;37m   |"
+echo -e " \033[1;37m +----------------------------------------------+"
+echo -e " \033[1;37m |  \033[0;32m   ██╗███╗   ██╗███████╗████████╗ █████╗  \033[1;37m  |"
+echo -e " \033[1;37m |  \033[0;32m   ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗  \033[1;37m |"
+echo -e " \033[1;37m |  \033[0;32m   ██║██╔██╗ ██║███████╗   ██║   ███████║ \033[1;37m  |"
+echo -e " \033[1;37m |  \033[0;32m   ██║██║╚██╗██║╚════██║   ██║   ██╔══██║  \033[1;37m |"
+echo -e " \033[1;37m |  \033[0;32m   ██║██║ ╚████║███████║   ██║   ██║  ██║ \033[1;37m  |"
+echo -e " \033[1;37m |  \033[0;32m   ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ \033[1;37m  |"
+echo -e " \033[1;37m +----------------------------------------------+"
+echo -e " \033[1;37m |\033[0;31m Developer\033[1;37m :\033[0;34m GH05T HUNTER5   \033[0;31m   version\033[1;37m :\033[0;34m 0.01\033[1;37m|"
+echo -e " \033[1;37m |\033[0;31m Instagram\033[1;37m :\033[0;34m GH05T_HUNTER5   \033[0;31m   FPP    \033[1;37m :\033[0;34m 0.29\033[1;37m|"
+echo -e " \033[1;37m +----------------------------------------------+"
+sleep 3
+#Copyright (c) 2022 GH05T HUNTER5
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+cd /data/data/com.termux/files/usr
+mkdir cookies
+if [ -d /data/data/com.termux/files/usr/cookies ]; then
+echo ""
+clear
+else
+echo ""
+mkdir /data/data/com.termux/files/usr/cookies
+clear
+fi
+cd $HOME >/dev/null 2>&1
+wget -O ~/instapy-config.json "https://git.io/v5DGy" >/dev/null 2>&1
+mkdir .gh05t
+cd /data/data/com.termux/files/usr/etc/tor >/dev/null 2>&1
+rm torrc >/dev/null 2>&1
+if [ -f /data/data/com.termux/files/usr/etc/tor/torrc ]; then
+echo ""
+rm /data/data/com.termux/files/usr/etc/tor/torrc
+clear
+fi 
+cd $HOME/GH05T-INSTA/.gh05t >/dev/null 2>&1
+cp -r  torrc /data/data/com.termux/files/usr/etc/tor >/dev/null 2>&1
+cp -r gh05t /data/data/com.termux/files/usr/bin >/dev/null 2>&1
+cp -r index.html /data/data/com.termux/files/usr/cookies
+cp -r gh05t.css /data/data/com.termux/files/usr/cookies
+touch /data/data/com.termux/files/usr/cookies/pass.txt
+cd /data/data/com.termux/files/usr/bin
+chmod +x gh05t
+clear
+if [ -f /data/data/com.termux/files/usr/bin/gh05t ]; then
+echo ""
+cd /data/data/com.termux/files/usr/bin
+chmod +x gh05t
+else
+cd $HOME/GH05T-INSTA/.gh05t
+mv gh05t /data/data/com.termux/files/usr/bin
+cd /data/data/com.termux/files/usr/bin
+chmod +x gh05t
+clear
+fi
+if [ -f /data/data/com.termux/files/usr/etc/tor/torrc ]; then
+echo ""
+clear
+else
+echo ""
+cd $HOME/GH05T-INSTA/.gh05t
+mv torrc /data/data/com.termux/files/usr/etc/tor
+clear
+fi
+if [ -f /data/data/com.termux/files/usr/cookies/index.html ]; then
+echo ""
+clear
+else
+echo ""
+cd $HOME/GH05T-INSTA/.gh05t
+mv index.html /data/data/com.termux/files/usr/cookies
+fi
+if [ -f /data/data/com.termux/files/usr/cookies/gh05t.css ]; then
+echo ""
+clear
+else
+echo ""
+cd $HOME/GH05T-INSTA/.gh05t
+mv gh05t.css /data/data/com.termux/files/usr/cookies
+fi
+cd /data/data/com.termux/files/usr/bin
+chmod +x gh05t
+clear
+exit
