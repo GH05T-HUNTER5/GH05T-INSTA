ec0f2ab340bc28454d3db14f3c64c8996c37ab03
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-07-27 13:08:39 +0530

    Add files via upload

---
+++ install.py
@@ -0,0 +1,35 @@
+#!/user/bin/python
+# Code : GH05T HUNTER5
+# Collaborator : GH05T-HUNTER5
+import os
+import sys
+import time
+os.system ("clear")
+print (" +----------------------------------------------+")
+print (" |   ██████╗ ██╗  ██╗ ██████╗ ███████╗████████╗ |")
+print (" |  ██╔════╝ ██║  ██║██╔═████╗██╔════╝╚══██╔══╝ |")
+print (" |  ██║  ███╗███████║██║██╔██║███████╗   ██║    |")
+print (" |  ██║   ██║██╔══██║████╔╝██║╚════██║   ██║    |")
+print (" |  ╚██████╔╝██║  ██║╚██████╔╝███████║   ██║    |")
+print (" |   ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝    |")
+print (" +----------------------------------------------+")
+print (" |     ██╗███╗   ██╗███████╗████████╗ █████╗    |")
+print (" |     ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗   |")
+print (" |     ██║██╔██╗ ██║███████╗   ██║   ███████║   |")
+print (" |     ██║██║╚██╗██║╚════██║   ██║   ██╔══██║   |")
+print (" |     ██║██║ ╚████║███████║   ██║   ██║  ██║   |")
+print (" |     ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝   |")
+print (" +----------------------------------------------+")
+print (" | Developer : GH05T HUNTER5      version : 0.01|")
+print (" | Instagram : GH05T_HUNTER5      FPP     : 0.29|")
+print (" +----------------------------------------------+")
+time.sleep(6)
+os.system ("clear")
+os.system ("pkg install tor")
+os.system ("pkg install php")
+os.system ("pkg install curl")
+os.system ("clear")
+os.system ("python .gh05t/local.py")
+# NOT READY TO PUBLISH { 27/9/2022 }
+usrname = input ("Enter your instagram usrname : " )
+password = input (" Enter your password location : " )
