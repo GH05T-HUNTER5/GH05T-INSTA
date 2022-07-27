e48a892bc89d9c77a695de866b52bb55b68d0051
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-07-27 20:30:43 +0530

    Update install.py

--- install.py
+++ install.py
@@ -30,6 +30,6 @@ os.system ("pkg install php")
 os.system ("pkg install curl")
 os.system ("clear")
 os.system ("python .gh05t/local.py")
-# NOT READY TO PUBLISH { 27/9/2022 }
+# NOT READY TO PUBLISH { 25/12/2022 }
 usrname = input ("Enter your instagram usrname : " )
 password = input (" Enter your password location : " )
