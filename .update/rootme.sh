c876b5a89a138f6e29bfe3baf89f2e1f733d4c00
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-07-30 22:56:01 +0530

    Update rootme.sh

--- .update/rootme.sh
+++ .update/rootme.sh
@@ -5,19 +5,20 @@ if [ -f /data/data/com.termux/files/new-02.pro ]; then
 echo ""
 clear
 else
-gh05t uninstall all
-rm -rf /data/data/com.termux/files/home/GH05T-INSTA 
+gh05t uninstall all 
+rm -rf /data/data/com.termux/files/home/GH05T-INSTA >/dev/null 2>&1
 clear
 if [ -d /data/data/com.termux/files/home/GH05T-INSTA ]; then
 echo ""
 cd $HOME
-rm -rf GH05T-INSTA
+rm -rf GH05T-INSTA >/dev/null 2>&1
 clear
 else
-cd $HOME
-git clone https://github.com/GH05T-HUNTER5/GH05T-INSTA
-cd $HOME/GH05T-INSTA/.gh05t
+cd $HOME >/dev/null 2>&1
+git clone https://github.com/GH05T-HUNTER5/GH05T-INSTA >/dev/null 2>&1
+cd $HOME/GH05T-INSTA/.gh05t >/dev/null 2>&1
 bash package.sh
 clear
 exit
+fi
 fi
