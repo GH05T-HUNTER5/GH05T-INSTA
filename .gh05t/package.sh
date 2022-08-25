3e0839a7107880489196b96dfebd05412ef0de45
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-08-25 18:18:39 +0530

    Update package.sh

--- .gh05t/package.sh
+++ .gh05t/package.sh
@@ -136,9 +136,7 @@ echo ""
 pkg install python -y >/dev/null 2>&1
 clear
 fi
-pkg install wget 
-clear
-pkg install wget -y
+clear 
 echo "Please Wait..."
 dpkg --configure -a 
 pip install --upgrade pip >/dev/null 2>&1
