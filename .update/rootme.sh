8c471363427ec6da52340bab1336efb13283cc12
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-08-26 10:50:40 +0530

    Update rootme.sh

--- .update/rootme.sh
+++ .update/rootme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo ""
 clear
-if [ -f /data/data/com.termux/files/new-03.pro ]; then
+if [ -f /data/data/com.termux/files/new-04.pro ]; then
 echo ""
 clear
 echo " No Update Found"
@@ -9,20 +9,13 @@ sleep 3
 clear
 exit 
 else
+echo " Please Wait Updating..."
+sleep 2
 gh05t uninstall all 
-rm -rf /data/data/com.termux/files/home/GH05T-INSTA >/dev/null 2>&1
-clear
-if [ -d /data/data/com.termux/files/home/GH05T-INSTA ]; then
-echo ""
-cd $HOME
-rm -rf GH05T-INSTA >/dev/null 2>&1
-clear
-else
 cd $HOME >/dev/null 2>&1
 git clone https://github.com/GH05T-HUNTER5/GH05T-INSTA >/dev/null 2>&1
 cd $HOME/GH05T-INSTA/.gh05t >/dev/null 2>&1
 bash package.sh
 clear
 exit
-fi
 fi
