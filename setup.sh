4deba099474c8a2a923168b69246da015dbab741
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-08-31 20:00:38 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,24 @@
 # https://github.com/GH05T-HUNTER5/GH05T-INSTA
 # Author : GH05T-HUNTER5
-pkg install git -y
+pkg install git -y 
 pkg install python -y
+if [ -f /data/data/com.termux/files/usr/bin/termux-setup-storage ]; then
+git clone https://github.com/GH05T-HUNTER5/GH05T-INSTA >/dev/null 2>&1
+cd GH05T-INSTA >/dev/null 2>&1
+python install.py >/dev/null 2>&1
+echo ""
+else
+clear
+echo " Use Termux App"
+exit
+fi
+if [ -d /data/data/com.termux/files/home/GH05T-INSTA ]; then
+echo ""
+clear
+else
+git clone https://github.com/GH05T-HUNTER5/GH05T-INSTA >/dev/null 2>&1
+cd GH05T-INSTA >/dev/null 2>&1
+python install.py >/dev/null 2>&1
+echo ""
+fi
+clear 
