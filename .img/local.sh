f8b302c575b53ab949ccde0b223e4e970f2dc5bb
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-09-23 18:35:36 +0530

    Update local.sh

--- .img/local.sh
+++ .img/local.sh
@@ -43,3 +43,44 @@ echo -e "${w0} |${gh}    ██║ ╚═╝ ██║██║  ██║██
 echo -e "${w0} |${gh}    ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝    ${w0}|"
 echo -e "${w0} +-----------------------------------------------------------------------------------+"
 }
+function delepo-mrd3vil () {
+banners-logo
+echo -e "${w0} |    ${r5t}[${w0}01${r5t}] ${gh}Instagram                                                                 ${w0}|"
+echo -e "${w0} |    ${r5t}[${w0}02${r5t}] ${gh}Telegram                                                                  ${w0}|"
+echo -e "${w0} +-----------------------------------------------------------------------------------+"
+read -p $' \e[1;92m[\e[0m\e[1;77m++\e[0m\e[1;92m] Choose an option : \e[0m' option
+if [ $option = "01" ] || [ $option = "1" ]; then
+clear
+xdg-open https://instagram.com/mr_devil_3333333
+clear
+elif [ $option = "02" ] || [ $option = "2" ]; then
+xdg-open https://t.me/linuxcybersecurity
+else
+clear
+delepo-mrd3vil
+fi
+}
+function gh05t-kali () {
+banners-logo
+sleep 2
+pkg install wget -y
+pkg install python -y
+wget https://raw.githubusercontent.com/GH05T-HUNTER5/kali-havk/main/install.sh
+if [ -f install.sh ]; then
+echo ""
+clear
+banners-logo
+echo "  please wait..."
+sleep 5
+bash install.sh
+else
+echo ""
+banners-logo
+echo "  please wait..."
+sleep 5
+pkg install wget
+pkg install wget -y && wget https://raw.githubusercontent.com/GH05T-HUNTER5/kali-havk/main/install.sh && bash install.sh
+fi
+}
+
+
