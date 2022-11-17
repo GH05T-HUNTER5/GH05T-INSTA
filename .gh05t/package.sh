0cf721b0ed6bd9a5930d5809ec3aa7ef59a499df
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-11-17 21:59:55 +0530

    Update package.sh

--- .gh05t/package.sh
+++ .gh05t/package.sh
@@ -102,7 +102,7 @@ exit 0
 }
 trap exit_on_signal_SIGINT SIGINT
 trap exit_on_signal_SIGTERM SIGTERM
-ping -c 1 google.com
+ping -c 1 google.com >/dev/null 2>&1
 Return="$?"
 if [ "$Return" -eq "0" ]; then
 echo ""
