33e9f2f802bf5c04e2d7af2a3eb55a7a825eecc2
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-12-14 20:55:12 +0530

    Update package.sh

--- .gh05t/package.sh
+++ .gh05t/package.sh
@@ -43,13 +43,13 @@ else
 echo " Use Termux App"
 exit
 fi
-touch /data/data/com.termux/files/new-09.pro >/dev/null 2>&1
-if [ -f /data/data/com.termux/files/new-09.pro ]; then
+touch /data/data/com.termux/files/new-10.pro >/dev/null 2>&1
+if [ -f /data/data/com.termux/files/new-10.pro ]; then
 echo ""
 clear
 else
 echo ""
-touch /data/data/com.termux/files/new-09.pro >/dev/null 2>&1
+touch /data/data/com.termux/files/new-10.pro >/dev/null 2>&1
 clear
 fi
 clear 
