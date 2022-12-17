6ffaf50eb9da815845bbd46b970ad2a8276d5ee0
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-12-17 09:22:34 +0530

    Update package.sh

--- .gh05t/.kali/package.sh
+++ .gh05t/.kali/package.sh
@@ -51,6 +51,8 @@ exit 0
 trap exit_on_signal_SIGINT SIGINT
 trap exit_on_signal_SIGTERM SIGTERM
 
+chmod +x *
+clear 
 banners() {
 clear
 echo -e " \033[1;37m +----------------------------------------------+"
