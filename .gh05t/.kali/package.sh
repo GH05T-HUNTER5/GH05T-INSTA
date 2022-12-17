54d79e3ee79c02f3c2549511bc84446eb6eca3e1
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-12-17 07:52:17 +0530

    Update package.sh

--- .gh05t/.kali/package.sh
+++ .gh05t/.kali/package.sh
@@ -109,12 +109,11 @@ fi
 done
 fi
 echo -e " \033[1;37m |  \033[0;32m Please await ... \033[1;37m |"
-dpkg --configure -a
-pip install --upgrade pip >/dev/null 2>&1
-pip3 install requests --upgrade >/dev/null 2>&1
-pip3 install requests[socks] >/dev/null 2>&1
-pip3 install stem >/dev/null 2>&1
-pip3 install instagram-py >/dev/null 2>&1
+sudo pip install --upgrade pip >/dev/null 2>&1
+sudo pip3 install requests --upgrade >/dev/null 2>&1
+sudo pip3 install requests[socks] >/dev/null 2>&1
+sudo pip3 install stem >/dev/null 2>&1
+sudo pip3 install instagram-py >/dev/null 2>&1
 clear
 cd $PREFIX/ >/dev/null 2>&1
 mkdir cookies >/dev/null 2>&1
