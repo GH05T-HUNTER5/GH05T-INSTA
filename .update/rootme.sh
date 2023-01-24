48796aebdc31b99bfac52884bb84c4cb04401097
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-01-24 14:18:52 +0530

    Update rootme.sh

--- .update/rootme.sh
+++ .update/rootme.sh
@@ -16,6 +16,7 @@ cd $HOME >/dev/null 2>&1
 git clone https://github.com/GH05T-HUNTER5/GH05T-INSTA >/dev/null 2>&1
 cd $HOME/GH05T-INSTA/.gh05t >/dev/null 2>&1
 bash package.sh
+rm -rf /data/data/com.termux/files/home/GH05T-INSTA >/dev/null 2>&1
 clear
 exit
 fi
